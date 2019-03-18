19aef2da838e571c85ca3aa88edf6519bbb90c05
nayabdckap <nayabsamar@dckap.com>
2019-03-18 11:32:08 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -14,11 +14,16 @@ export default class Create extends Component {
             person_name: '',
             business_name: '',
             business_gst_number: '',
-            data_test: {
-                person: [],  // use separate component
-                business: [],
-                gst: []
-            }
+            current_state: '',
+            index_test: '',
+            // data_test: {
+            //     person: [],
+            //     business: [],
+            //     gst: []
+            // }
+            data_test: [
+                {person: "", business: "", gst: ""}
+            ]
         }
     }
 
@@ -41,41 +46,70 @@ export default class Create extends Component {
     }
 
     onSubmit(e) {
-        e.preventDefault();
-        var collection_temp = this.state.data_test;
-        collection_temp.person.push(this.state.person_name);
-        collection_temp.business.push(this.state.business_name);
-        collection_temp.gst.push(this.state.business_gst_number);
-        this.setState({
-            person_name: '',
-            business_name: '',
-            business_gst_number: '',
-            data_test: collection_temp
-        })
+        e.preventDefault(); // read about it -- overwrites default methods like onClick
+        if (this.state.current_state == "Update") {
+            var tempArr = this.state.data_test
+            tempArr[this.state.index_test].person = this.state.person_name
+            tempArr[this.state.index_test].business = this.state.business_name
+            tempArr[this.state.index_test].gst = this.state.business_gst_number
+            this.setState({
+                person_name: '',
+                business_name: '',
+                business_gst_number: '',
+                data_test: tempArr
+            })
+        } else {
+            var collection_temp = this.state.data_test;
+            var tempHash = {
+                person: this.state.person_name,
+                business: this.state.business_name,
+                gst: this.state.business_gst_number
+            }
+            collection_temp.push(tempHash)
+
+            // array method:
+            // collection_temp.person.push(this.state.person_name);
+            // collection_temp.business.push(this.state.business_name);
+            // collection_temp.gst.push(this.state.business_gst_number);
+            this.setState({
+                person_name: '',
+                business_name: '',
+                business_gst_number: '',
+                data_test: collection_temp
+            })
+        }
     }
 
     delete_item(index) {
-        // var item = this.state
-        // item.splice(index, 1);
         console.log('hey')
-        // debugger
-        var test_var = this.state.data_test
-        test_var.person = this.state.data_test.person.filter((name) => name != this.state.data_test.person[index])
-        test_var.business = this.state.data_test.business.filter((name) => name != this.state.data_test.business[index])
-        test_var.gst = this.state.data_test.gst.filter((name) => name != this.state.data_test.gst[index])
-        // var item = this.state.data_test.person.filter((name) => {
-        //     return name != this.state.data_test.person[index]
-        // })
+
+        var tempArr = this.state.data_test
+        if (index > -1)
+            tempArr.splice(index, 1)
+
+
+        // var test_var = this.state.data_test
+        // test_var.person = this.state.data_test.person.filter((name) => name != this.state.data_test.person[index])
+        // test_var.business = this.state.data_test.business.filter((name) => name != this.state.data_test.business[index])
+        // test_var.gst = this.state.data_test.gst.filter((name) => name != this.state.data_test.gst[index])
         this.setState({
-            data_test: test_var
+            // data_test: test_var
+            data_test: tempArr
         });
     }
 
-    update_item(e) {
-        // var item = this.state.data_test;
-        // this.setState({
-        //     data_test: item
-        // })
+    update_item(index) {
+        if (index > -1) {
+            this.state.current_state = "Update"
+            this.state.index_test = index
+            var tempArr = this.state.data_test
+            this.state.person_name = tempArr[index].person
+            this.state.business_name = tempArr[index].business
+            this.state.business_gst_number = tempArr[index].gst
+            this.setState({
+                data_test: tempArr
+            })
+        }
     }
 
     render() {
@@ -109,7 +143,9 @@ export default class Create extends Component {
                         />
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="Register Business" className="btn btn-primary"/>
+                        {(this.state.current_state === "Update") ?
+                            <input type="submit" value="Update Business" className="btn btn-warning"/> :
+                            <input type="submit" value="Register Business" className="btn btn-primary"/>}
                     </div>
                 </form>
                 <NewComponent params_new={this.state.data_test} deleteVal={this.delete_item}
@@ -125,7 +161,8 @@ class NewComponent extends Component {
     }
 
     render() {
-        if (this.props.params_new.person.length > 0) {
+        if (this.props.params_new.length > 1) {
+            console.log(this.props.params_new)
             return (
                 <div>
                     <table className="table">
@@ -140,12 +177,34 @@ class NewComponent extends Component {
                         </thead>
                         <tbody>
 
-                        {(this.props.params_new.person).map((item, index) => {
+                        {/*{(this.props.params_new.person).map((item, index) => {*/}
+                        {/*return (*/}
+                        {/*<tr key={index}>*/}
+                        {/*<td>{this.props.params_new.person[index]}</td>*/}
+                        {/*<td>{this.props.params_new.business[index]}</td>*/}
+                        {/*<td>{this.props.params_new.gst[index]}</td>*/}
+                        {/*<td><input type="button"*/}
+                        {/*className="btn btn-warning"*/}
+                        {/*value="Edit"*/}
+                        {/*onClick={() => this.props.editVal(index)}*/}
+                        {/*/></td>*/}
+                        {/*<td><input type="button"*/}
+                        {/*className="btn btn-danger"*/}
+                        {/*value="Remove"*/}
+                        {/*method="delete"*/}
+                        {/*onClick={() => this.props.deleteVal(index)}*/}
+                        {/*/></td>*/}
+                        {/*</tr>*/}
+                        {/*)*/}
+                        {/*}*/}
+                        {/*)}*/}
+
+                        {(this.props.params_new).map((item, index) => {
                                 return (
                                     <tr key={index}>
-                                        <td>{this.props.params_new.person[index]}</td>
-                                        <td>{this.props.params_new.business[index]}</td>
-                                        <td>{this.props.params_new.gst[index]}</td>
+                                        <td>{item.person}</td>
+                                        <td>{item.business}</td>
+                                        <td>{item.gst}</td>
                                         <td><input type="button"
                                                    className="btn btn-warning"
                                                    value="Edit"
@@ -168,7 +227,7 @@ class NewComponent extends Component {
             )
         } else {
             return (
-                <div>Void</div>
+                <div align="center">No Records Available. Add a Business above.</div>
             )
         }
 
